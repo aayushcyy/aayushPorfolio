5dc24e7d2653cddf40a6837db24c671445297f10
Aayush Chaudhary <aayushcyy@gmail.com>
2025-07-10 18:28:10 +0530

    update: ui layout and styling for hero section

--- app/Components/HeroSection.jsx
+++ app/Components/HeroSection.jsx
@@ -6,25 +6,31 @@ import { ArrowDownToLine } from "lucide-react";
 
 export default function HeroSection() {
   return (
-    <div className="w-full md:h-[90vh] flex justify-center items-center text-white font-funnel px-40">
+    <div className="w-full md:h-[85vh] flex justify-center items-center text-white font-funnel px-40">
       <div className="md:flex w-full md:justify-between">
-        <div className="flex flex-col gap-2 justify-end">
-          <div>
-            <Image
-              src={profilePic}
-              width={600}
-              height={600}
-              alt="Aayush Chaudhary"
-              className="w-40 rounded-lg"
-            />
+        <div className="flex flex-col gap-2 justify-between">
+          <div className="flex items-center gap-2">
+            <span className="w-2 h-2 bg-white"></span>
+            <p className="uppercase">About me</p>
           </div>
-          <div className="pl-1">Raipur, India</div>
+          <div className="flex flex-col gap-2 justify-end">
+            <div>
+              <Image
+                src={profilePic}
+                width={600}
+                height={600}
+                alt="Aayush Chaudhary"
+                className="w-40 rounded-lg"
+              />
+            </div>
+            <div className="pl-1">Raipur, India</div>
+          </div>
         </div>
         <div className="w-[55%] h-full flex flex-col items-start gap-13">
           <p className="text-5xl ">
             Hi, I'm Aayush. <br /> A Full Stack Developer.
           </p>
-          <p className="text-lg font-thin w-[65%]">
+          <p className="text-xl font-thin w-[65%]">
             I'm a passionate web developer and designer crafting fast, scalable,
             and user-friendly web applications. I help you make a lasting
             impression online through clean code and impactful design.
